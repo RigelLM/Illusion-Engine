621e633c2a1d6376aab17a874f480ddadc071bb8
Rigel <112126663+RigelLM@users.noreply.github.com>
2026-03-07 07:38:32 -0500

    Fix building failure

--- Illusion/src/Engine/ImGui/ImGuiBuild.cpp
+++ Illusion/src/Engine/ImGui/ImGuiBuild.cpp
@@ -1,10 +1,11 @@
-/*****************************************************************//**
+﻿/*****************************************************************//**
  * \file   ImGuiBuild.cpp
  * \brief  imgui building options
  * 
  * \author Mian Li
  * \date   October 2024
  *********************************************************************/
+#include "pch.h"
 
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
 #include "backends/imgui_impl_opengl3.cpp"
